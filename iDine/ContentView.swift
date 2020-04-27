0479ad00662665ed0ac1c2dd97dad39bb46da66f
Benjamin Dirgo <bdirgo@gmail.com>
2020-04-26 17:18:53 -0700

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Benjamin Dirgo on 4/26/20.
+//  Copyright © 2020 Benjamin Dirgo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
